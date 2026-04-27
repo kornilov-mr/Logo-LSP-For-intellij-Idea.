ae387f12335d36289af93a47c87a4385910f0e71
korni <Kornilov.mr@gmail.com>
2026-04-27 20:55:36 +0200

    Added: Parser tests for Expressions

--- src/test/java/ast/ConvertToASTNoArityTests.java
+++ src/test/java/ast/ConvertToASTNoArityTests.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class ConvertToASTNoArityTests extends ParserAbstractTest {
 
     private final File logoSimple1 = new File("src/test/resources/ast/programs/validLogo/logoSimple1.logo");
+    private final File logoExpressions = new File("src/test/resources/ast/programs/validLogo/logoExpressions.logo");
     private final File logoVariableReference = new File("src/test/resources/ast/programs/validLogo/logoVariableReference.logo");
     private final File logoArithmetic = new File("src/test/resources/ast/programs/validLogo/logoArithmetic.logo");
     private final File logoRepeat = new File("src/test/resources/ast/programs/validLogo/logoRepeat.logo");
@@ -23,6 +24,10 @@ public class ConvertToASTNoArityTests extends ParserAbstractTest {
     @Test
     public void convertSimpleLogoWithNoArityResolve() throws IOException {
         super.assertMatchesExpectedParsing(logoSimple1);
+    }
+    @Test
+    public void convertLogoExpressionsWithNoArityResolve() throws IOException {
+        super.assertMatchesExpectedParsing(logoExpressions);
     }
     @Test
     public void convertLogoVariableReference() throws IOException {
